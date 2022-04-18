06280d05a8d707332fad799a33c9113c7ebdc90c
aslusaf <aslusaf@live.com>
2022-04-18 10:29:25 -0600

    DB Update

--- Airports/src/main/java/com/skilldistillery/airports/AirportsApplication.java
+++ Airports/src/main/java/com/skilldistillery/airports/AirportsApplication.java
@@ -2,15 +2,18 @@ package com.skilldistillery.airports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.skilldistillery.airports.data.AirportDaoJpaImpl;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AirportsApplication {
+public class AirportsApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AirportsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirportsApplication.class, args);
-		
 	}
 
 }
